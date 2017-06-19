86b2176074a181a80ec77c1a2062db138ccd6e4c
navaneethan <navaneethansoundar@gmail.com>
2017-06-19 18:39:34 +0530

    Update myindex.js

--- myindex.js
+++ myindex.js
@@ -2,54 +2,54 @@ var app = angular.module("myApp", ["ngRoute",'ConApp','IntApp']);
 app.config(function($routeProvider,$locationProvider) {
     $routeProvider
     .when("/aboutme", {
-        templateUrl : "views/about/mot.html"
+        templateUrl : "./views/about/mot.html"
 
     })
     
      
      .when("/Home", {
-        templateUrl : "views/home/home.html"
+        templateUrl : "./views/home/home.html"
 
     })
        .when("/qualification", {
-        templateUrl : "views/qualification/quali.html"
+        templateUrl : "./views/qualification/quali.html"
 
     })
         .when("/contactme", {
-        templateUrl : "views/contact/contact.html",
+        templateUrl : "./views/contact/contact.html",
         controller:"myCtrl"
 
     })
           .when("/hobbies", {
-        templateUrl : "views/hobby/hoby.html"
+        templateUrl : "./views/hobby/hoby.html"
 
     })
           .when("/workshop", {
-        templateUrl : "views/workshop/workshop.html"
+        templateUrl : "./views/workshop/workshop.html"
 
     })
           .when("/interpersonal", {
-        templateUrl : "views/interpersonal/inter.html",
+        templateUrl : "./views/interpersonal/inter.html",
         controller:"showCtrl"
     })
            .when("/extracurricular", {
-        templateUrl : "views/extracurricular/extra.html"
+        templateUrl : "./views/extracurricular/extra.html"
 
     })
 
            .when("/software", {
-        templateUrl : "views/software/software.html"
+        templateUrl : "./views/software/software.html"
 
     })
            .when("/leadership", {
-        templateUrl : "views/leadership/leader.html"
+        templateUrl : "./views/leadership/leader.html"
 
     })
       $locationProvider.html5Mode(true);
 });
       
      app.controller('nav', function($scope, $http) {
-     $http.get("/nav.json")
+     $http.get("./nav.json")
      .then(function (response) {
       $scope.navbar = response.data.navmenu;
        $scope.drop1 = response.data.navdrop1;
