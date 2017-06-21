cbc2607582887f7c04aa8cbbe0727666ee5e849f
navaneethan <navaneethansoundar@gmail.com>
2017-06-21 18:21:47 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,9 @@
+angular.module('ConApp',[])
+.controller('myCtrl',function($scope,$window){
+        $scope.myFun=function(){
+        alert("Thanks for ur feedback")  
+        }
+
+        $window.location.href("http://localhost/resume.html");
+        
+      });
